8031d1182a1fb4bd6c9839b570e3744029ddef15
salmafele <salmafele8@gmail.com>
2020-01-26 21:41:59 -0500

    Update ApiKeys.swift

--- BusinessLocator_WatchApp/BusinessLocator_WatchApp WatchKit Extension/Data/ApiKeys.swift
+++ BusinessLocator_WatchApp/BusinessLocator_WatchApp WatchKit Extension/Data/ApiKeys.swift
@@ -8,7 +8,7 @@
 
 class ApiKeys {
 
-    static let googleApiKey = "AIzaSyC8f2dhxdBIb9vkeUhlS4csUEpLlvsc5dk"
+    static let googleApiKey = YourGoogleApiKey
     
-    static let yelpApiKey = "Bearer jhzPe8Pyf0-uMZE1zvM3ROUZBq8Z7vauFTSxf1TbwlZOcPUHCRjDA9zksocZ67RUc_BdE9a0Fy8jynh9ZuNn3p0To7zC_ecf6ZiyIWWujq0LTYGGa5QSfII1FNryXXYx"
+    static let yelpApiKey = YourYelpApiKey
 }
